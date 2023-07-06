b8e57705f2d34eed870f5c7abca6d21635c22a60
Ann <ermolaeva-ann@mail.ru>
2023-07-06 23:40:05 +0300

    Add project files.

---
+++ Networking/Network.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#ifdef _WIN32
+#define _WIN32_WINNT 0x0A00
+#endif
+#define ASIO_STANDALONE
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
+int main()
+{
+	asio::error_code ec;
+	asio::io_context context;
+	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.216.34", ec), 80);
+	asio::ip::tcp::socket socket(context);
+	socket.connect(endpoint, ec);
+
+	if (!ec)
+	{
+		std::cout << "Connected!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Failed to connect to address:\n" << ec.message() << std::endl;
+	}
+
+	system("pause");
+
+	return 0;
+}
